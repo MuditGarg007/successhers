4a27cdfad5cef767b97d795ec0706e57628c7e0f
Kairav Mittal <kairavmittal@gmail.com>
2025-06-19 04:44:09 +0530

    minor bug fixes

--- src/app/dashboard/learning/kirti/page.tsx
+++ src/app/dashboard/learning/kirti/page.tsx
@@ -2,7 +2,14 @@ import KirtiAI from "@/components/kirti-ai";
 
 function Page() {
   return (
-    <div>
+    <div
+      style={{
+        borderTopLeftRadius: 8,
+        borderTopRightRadius: 8,
+        borderBottomLeftRadius: 8,
+        borderBottomRightRadius: 0, // Only this corner is different
+      }}
+    >
       <KirtiAI />
     </div>
   );
